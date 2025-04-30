84094a617adccbd7f5965286ada25cd4305c9adf
Женёк <muratov_yevgeniy@vk.com>
2025-04-30 12:37:38 +0300

    final commit

---
+++ src/test/java/com/myuniversity/coursework/examination_system/ExaminationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myuniversity.coursework.examination_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
